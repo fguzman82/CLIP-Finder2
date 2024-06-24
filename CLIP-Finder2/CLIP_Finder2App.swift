b8b6b6b018c1a6b82efb8c10ea91f44aa8d1c526
fabio <fabio.guzmanf@upb.edu.co>
2024-06-24 16:45:14 -0500

    Initial Commit

---
+++ CLIP-Finder2/CLIP_Finder2App.swift
@@ -0,0 +1,17 @@
+//
+//  CLIP_Finder2App.swift
+//  CLIP-Finder2
+//
+//  Created by Fabio Guzman on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CLIP_Finder2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLIP-Finder2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CLIP-Finder2
+//
+//  Created by Fabio Guzman on 24/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
